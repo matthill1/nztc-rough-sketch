ad75eeff2951c713d4bbb2b9fea7e730b49cc6a5
matthill1 <matthill50@hotmail.co.uk>
2024-01-31 09:42:53 +0000

    Rough sketch of menu w/ animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { useClickAway } from "react-use";
 import { useRef } from "react";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Squash as Hamburger } from "hamburger-react";
 import { routes } from "../routes";
 
@@ -16,6 +17,45 @@ export const Nav = () => {
     <div id='menu' ref={ref}>
       <span>MENU</span>
       <Hamburger toggled={isOpen} size={24} toggle={setOpen} />
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="menu_container"
+          >
+            <ul className="">
+              {routes.map((route, idx) => {
+                const { Icon } = route;
+
+                return (
+                  <motion.li
+                    initial={{ scale: 0, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                      delay: 0.1 + idx / 10,
+                    }}
+                    key={route.title}
+                    className="nav_item"
+                  >
+                    <a
+                      onClick={() => setOpen((prev) => !prev)}
+                      href={route.href}
+                    >
+                      {route.title}
+                    </a>
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
